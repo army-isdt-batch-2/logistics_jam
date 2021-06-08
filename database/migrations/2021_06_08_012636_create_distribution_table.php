eb3aa9cc54bf6f58c45eaca92871ca91b64ee48f
jagdadler <agila244@gmail.com>
2021-06-08 10:03:07 +0800

    initial setup

---
+++ database/migrations/2021_06_08_012636_create_distribution_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistributionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distribution', function (Blueprint $table) {
+            $table->id();
+            $table->string('requestor_name');
+            $table->integer('requestor_contact');
+            $table->string('purpose');
+            $table->string('asset_id');
+            $table->integer('quantity');
+            $table->enum('status', [
+                'processing',
+                'declined',
+                'distributed'
+            ]);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distribution');
+    }
+}
---
+++ database/migrations/2021_06_08_012707_create_deliveries_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->string('distribution_id');
+            $table->string('transportation_id');
+            $table->date('date_distributed');
+            $table->enum('status',[
+                'delivered',
+                'returned'
+            ]);
+            $table->timestamps();
+        });
+    }
+
+
+
+
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
